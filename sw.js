4a8023e34b0032fd65a6c5b1db1e6890799d2833
therafermetures-ops <therafermetures@gmail.com>
2026-04-23 11:17:13 +0200

    Remove service worker code from sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-const cacheName = 'thera-v4';
 
-self.addEventListener('install', (event) => {
-    // Force le service worker à s'activer immédiatement sans attendre
-    self.skipWaiting();
-    console.log("THERA SW: Installé");
-});
-
-self.addEventListener('activate', (event) => {
-    console.log("THERA SW: Activé");
-});
-
-self.addEventListener('fetch', (event) => {
-    // Stratégie simple : on va chercher sur le réseau, si ça échoue on regarde le cache
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
-});
